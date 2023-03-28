6e683e412f1a221d90a2f585b49cf0595b63d667
Moushes <adrian123van@gmail.com>
2023-03-28 13:06:07 +0200

    Base
    tablero y camara bien

---
+++ Assets/TacticsCamera.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TacticsCamera : MonoBehaviour
+{
+    public void RotateLeft()
+    {
+        transform.Rotate(Vector3.up, 90, Space.Self);
+        //Camera.main.transform.Translate(-686,-256,6);
+    }
+
+    public void RotateRight()
+    {
+        transform.Rotate(Vector3.up, -90, Space.Self);
+    }
+}
