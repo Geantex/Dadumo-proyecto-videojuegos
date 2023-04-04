83290aa6c150471e9be13ce4361e8c980965afe4
Mario Climent <mclijim@epsg.upv.es>
2023-04-04 05:27:40 +0200

    Modificaciones Para juego Base
    Se incluye lo anteriormente nombrado

--- Assets/Scripts/TacticsMove.cs
+++ Assets/Scripts/TacticsMove.cs
@@ -1,12 +1,10 @@
-using System.Collections;
+ï»¿using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.UIElements;
 
-public class TacticsMove : MonoBehaviour
+public class TacticsMove : MonoBehaviour 
 {
-    //public bool turn = false;
+    public bool turn = false;
 
     List<Tile> selectableTiles = new List<Tile>();
     GameObject[] tiles;
@@ -31,17 +29,17 @@ public class TacticsMove : MonoBehaviour
     Vector3 jumpTarget;
 
     public Tile actualTargetTile;
-    public void Init(GameObject gameObject, Unit unit) 
+
+    protected void Init()
     {
         tiles = GameObject.FindGameObjectsWithTag("Tile");
 
-        halfHeight = gameObject.GetComponent<Collider>().bounds.extents.y;
+        halfHeight = GetComponent<Collider>().bounds.extents.y;
 
-        //GetCurrentTile(gameObject);
-        TurnManager.AddUnit(unit, this);
+        TurnManager.AddUnit(this);
     }
 
-    public void GetCurrentTile(GameObject gameObject)
+    public void GetCurrentTile()
     {
         currentTile = GetTargetTile(gameObject);
         currentTile.current = true;
@@ -51,11 +49,12 @@ public class TacticsMove : MonoBehaviour
     {
         RaycastHit hit;
         Tile tile = null;
-        if (Physics.Raycast(target.transform.position, -Vector3.up, out hit, 1)) 
+
+        if (Physics.Raycast(target.transform.position, -Vector3.up, out hit, 1))
         {
             tile = hit.collider.GetComponent<Tile>();
         }
-        Debug.Log(tile);
+
         return tile;
     }
 
@@ -70,10 +69,10 @@ public class TacticsMove : MonoBehaviour
         }
     }
 
-    public void FindSelectableTiles(GameObject gameObject)
+    public void FindSelectableTiles()
     {
         ComputeAdjacencyLists(jumpHeight, null);
-        GetCurrentTile(gameObject);
+        GetCurrentTile();
 
         Queue<Tile> process = new Queue<Tile>();
 
@@ -103,6 +102,7 @@ public class TacticsMove : MonoBehaviour
             }
         }
     }
+
     public void MoveToTile(Tile tile)
     {
         path.Clear();
@@ -117,44 +117,39 @@ public class TacticsMove : MonoBehaviour
         }
     }
 
-    public void Move(GameObject gameObject)
+    public void Move()
     {
         if (path.Count > 0)
         {
-            //Tile t = path.Peek();
             Tile t = path.Peek();
-            if (t != null) // Agrega esta verificación condicional para evitar acceder a una referencia nula
+            Vector3 target = t.transform.position;
+
+            //Calculate the unit's position on top of the target tile
+            target.y += halfHeight + t.GetComponent<Collider>().bounds.extents.y;
+
+            if (Vector3.Distance(transform.position, target) >= 0.05f)
             {
-                Vector3 target = t.transform.position;
+                bool jump = transform.position.y != target.y;
 
-                // Calculate the unit's position on top of the target tile (we are going to move it there!)
-                target.y += halfHeight + t.GetComponent<Collider>().bounds.extents.y;
-
-                if (Vector3.Distance(gameObject.transform.position, target) >= 0.05f)
+                if (jump)
                 {
-                    bool jump = gameObject.transform.position.y != target.y;
-
-                    if (jump)
-                    {
-                        Jump(target, gameObject);
-                    }
-                    else
-                    {
-                        CalculateHeading(target, gameObject);
-                        SetHorizontalVelocity();
-                    }
-
-                    gameObject.transform.forward = heading;
-                    gameObject.transform.position += velocity * Time.deltaTime;
+                    Jump(target);
                 }
                 else
                 {
-                    // Tile center reached
-                    gameObject.transform.position = target;
-                    path.Pop();
-
-                    
+                    CalculateHeading(target);
+                    SetHorizotalVelocity();
                 }
+
+                //Locomotion
+                transform.forward = heading;
+                transform.position += velocity * Time.deltaTime;
+            }
+            else
+            {
+                //Tile center reached
+                transform.position = target;
+                path.Pop();
             }
         }
         else
@@ -165,14 +160,15 @@ public class TacticsMove : MonoBehaviour
             TurnManager.EndTurn();
         }
     }
-    
+
     protected void RemoveSelectableTiles()
     {
         if (currentTile != null)
         {
             currentTile.current = false;
-            currentTile= null;
+            currentTile = null;
         }
+
         foreach (Tile tile in selectableTiles)
         {
             tile.Reset();
@@ -180,43 +176,44 @@ public class TacticsMove : MonoBehaviour
 
         selectableTiles.Clear();
     }
-    void CalculateHeading(Vector3 target, GameObject gameObject)
-    {
-        heading = target - gameObject.transform.position;
-        heading.Normalize();
 
+    void CalculateHeading(Vector3 target)
+    {
+        heading = target - transform.position;
+        heading.Normalize();
     }
-    void SetHorizontalVelocity()
+
+    void SetHorizotalVelocity()
     {
         velocity = heading * moveSpeed;
     }
 
-    void Jump(Vector3 target, GameObject gameObject)
+    void Jump(Vector3 target)
     {
         if (fallingDown)
         {
-            FallDownward(target, gameObject);
+            FallDownward(target);
         }
         else if (jumpingUp)
         {
-            JumpUpward(target, gameObject);
+            JumpUpward(target);
         }
         else if (movingEdge)
         {
-            MoveToEdge(gameObject);
+            MoveToEdge();
         }
         else
         {
-            PrepareJump(target, gameObject);
+            PrepareJump(target);
         }
     }
 
-    void PrepareJump(Vector3 target, GameObject gameObject)
+    void PrepareJump(Vector3 target)
     {
         float targetY = target.y;
         target.y = transform.position.y;
 
-        CalculateHeading(target, gameObject);
+        CalculateHeading(target);
 
         if (transform.position.y > targetY)
         {
@@ -224,7 +221,7 @@ public class TacticsMove : MonoBehaviour
             jumpingUp = false;
             movingEdge = true;
 
-            jumpTarget = gameObject.transform.position + (target - gameObject.transform.position) / 2.0f;
+            jumpTarget = transform.position + (target - transform.position) / 2.0f;
         }
         else
         {
@@ -234,46 +231,46 @@ public class TacticsMove : MonoBehaviour
 
             velocity = heading * moveSpeed / 3.0f;
 
-            float difference = targetY - gameObject.transform.position.y;
+            float difference = targetY - transform.position.y;
 
             velocity.y = jumpVelocity * (0.5f + difference / 2.0f);
         }
     }
 
-    void FallDownward(Vector3 target, GameObject gameObject)
+    void FallDownward(Vector3 target)
     {
         velocity += Physics.gravity * Time.deltaTime;
 
-        if (gameObject.transform.position.y <= target.y)
+        if (transform.position.y <= target.y)
         {
             fallingDown = false;
             jumpingUp = false;
             movingEdge = false;
 
-            Vector3 p = gameObject.transform.position;
+            Vector3 p = transform.position;
             p.y = target.y;
-            gameObject.transform.position = p;
+            transform.position = p;
 
             velocity = new Vector3();
         }
     }
 
-    void JumpUpward(Vector3 target, GameObject gameObject)
+    void JumpUpward(Vector3 target)
     {
         velocity += Physics.gravity * Time.deltaTime;
 
-        if (gameObject.transform.position.y > target.y)
+        if (transform.position.y > target.y)
         {
             jumpingUp = false;
             fallingDown = true;
         }
     }
 
-    void MoveToEdge(GameObject gameObject)
+    void MoveToEdge()
     {
-        if (Vector3.Distance(gameObject.transform.position, jumpTarget) >= 0.05f)
+        if (Vector3.Distance(transform.position, jumpTarget) >= 0.05f)
         {
-            SetHorizontalVelocity();
+            SetHorizotalVelocity();
         }
         else
         {
@@ -327,18 +324,16 @@ public class TacticsMove : MonoBehaviour
         return endTile;
     }
 
-    protected void FindPath(Tile target, GameObject gameObject)
+    protected void FindPath(Tile target)
     {
         ComputeAdjacencyLists(jumpHeight, target);
-        GetCurrentTile(gameObject);
+        GetCurrentTile();
 
         List<Tile> openList = new List<Tile>();
         List<Tile> closedList = new List<Tile>();
 
         openList.Add(currentTile);
         //currentTile.parent = ??
-        Debug.Log(target.transform.position);
-        Debug.Log(currentTile.transform.position);
         currentTile.h = Vector3.Distance(currentTile.transform.position, target.transform.position);
         currentTile.f = currentTile.h;
 
@@ -390,9 +385,13 @@ public class TacticsMove : MonoBehaviour
         Debug.Log("Path not found");
     }
 
-    
+    public void BeginTurn()
+    {
+        turn = true;
+    }
+
+    public void EndTurn()
+    {
+        turn = false;
+    }
 }
-
-
-
-    
