0afaf8a79f1c4b0b316c028e822ea87dd4f63acf
Mr. Beast <lavafrita@gmail.com>
2023-05-01 15:05:25 +0200

    Ya no es un arreglo rapido
    He reordenado como funcionan los eventos, ahora es mucho mas limpio

--- Assets/Encounters/Encounter.cs
+++ Assets/Encounters/Encounter.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Data;
+using System;
+using System.Reflection;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Encounter : MonoBehaviour
+[CreateAssetMenu()]
+public class Encounter : ScriptableObject
 {
-    // tienes el movil en la mochila
-
     // Estas propiedades serán las que, cuando carguemos un evento al azar, rellenen el canvas de eventos
     // Necesita un nombre y una descripción, obviamente
     // eventOptions será para las opciones que tienes para solucionar el evento (máximo 4 por evento)
@@ -15,13 +17,14 @@ public class Encounter : MonoBehaviour
     // he cambiado el nombre de evento a encuentro
     public string encounterName;
     public string encounterDescription;
-    // public List<EncounterOption> encounterOptions;
-    public Material encounterImage;
+    public Sprite encounterImage;
+    public string encounterButton1Text;
+    public string encounterButton2Text;
+    public string encounterButton3Text;
+    public string encounterButton4Text;
 
-    public string encounterButton1Text = null;
-    public string encounterButton2Text = null;
-    public string encounterButton3Text = null;
-    public string encounterButton4Text = null;
+    // Cojo un GameObject vacío que no esta en escena (un prefab...) y este GameObject tiene scripts!!! es una gran idea!
+    public GameObject buttonScripts;
 
     // cada nuevo Encuentro será un nuevo script que heredará de este script
 
@@ -32,43 +35,68 @@ public class Encounter : MonoBehaviour
 
     public void FillEncounterCanvas()
     {
-        GameObject imageSlot = GameObject.Find("EncounterImage");
-        GameObject nameSlot = GameObject.Find("EncounterTitle");
-        GameObject descriptionSlot = GameObject.Find("EncounterDescription");
-        GameObject button1TextSlot = GameObject.Find("EncounterButton1Text");
-        GameObject button2TextSlot = GameObject.Find("EncounterButton2Text");
-        GameObject button3TextSlot = GameObject.Find("EncounterButton3Text");
-        GameObject button4TextSlot = GameObject.Find("EncounterButton4Text");
-        GameObject button1 = GameObject.Find("EncounterButton1");
-        GameObject button2 = GameObject.Find("EncounterButton2");
-        GameObject button3 = GameObject.Find("EncounterButton3");
-        GameObject button4 = GameObject.Find("EncounterButton4");
+        // El panel que contiene todo
+        GameObject panelEncounter = GameObject.FindGameObjectWithTag("EncounterCanvas").gameObject.transform.Find("Panel").gameObject;
+
+        // texto, descripcion e imagen
+        GameObject imageSlot = panelEncounter.transform.Find("EncounterImage").gameObject;
+        GameObject nameSlot = panelEncounter.transform.Find("EncounterTitle").gameObject;
+        GameObject descriptionSlot = panelEncounter.transform.Find("EncounterDescription").gameObject;
+
+        // botones
+        GameObject button1 = panelEncounter.transform.Find("EncounterButton1").gameObject;
+        GameObject button2 = panelEncounter.transform.Find("EncounterButton2").gameObject;
+        GameObject button3 = panelEncounter.transform.Find("EncounterButton3").gameObject;
+        GameObject button4 = panelEncounter.transform.Find("EncounterButton4").gameObject;
+        GameObject button1TextSlot = button1.transform.Find("EncounterButton1Text").gameObject;
+        GameObject button2TextSlot = button2.transform.Find("EncounterButton2Text").gameObject;
+        GameObject button3TextSlot = button3.transform.Find("EncounterButton3Text").gameObject;
+        GameObject button4TextSlot = button4.transform.Find("EncounterButton4Text").gameObject;
+        
 
         nameSlot.GetComponent<Text>().text = encounterName;
         descriptionSlot.GetComponent<Text>().text = encounterDescription;
-        imageSlot.GetComponent<Image>().material = encounterImage;
+        imageSlot.GetComponent<Image>().sprite = encounterImage;
 
         // Puede que no hayan 4 opciones en todos los encuentros, así que vamos a hacer ifs para ver si hay texto que agregar al boton
-
-        if (encounterButton1Text != null)
+        //myScriptReference = go.GetComponent<MyScript>();
+        Debug.Log("Es hora de asignar!");
+        if (encounterButton1Text != null || encounterButton1Text != "")
         {
             button1TextSlot.GetComponent<Text>().text = encounterButton1Text;
+            try
+            {
+                // veamos si funciona????
+                button1.GetComponent<Button>().onClick.AddListener(() => buttonScripts.GetComponent<MonoBehaviour>().GetType().GetMethod("functionButton1").Invoke(buttonScripts.GetComponent<MonoBehaviour>(), null));
+            }
+            catch
+            {
+                Debug.LogError("Button 1 no tiene función asignada!");
+            }
         }
         else
         {
             button1.SetActive(false);
         }
         
-        if (encounterButton2Text != null)
+        if (encounterButton2Text != null || encounterButton2Text != "")
         {
             button2TextSlot.GetComponent<Text>().text = encounterButton2Text;
+            try
+            {
+                // poner funcion aqui
+            }
+            catch
+            {
+                Debug.LogError("Button 2 no tiene función asignada!");
+            }
         }
         else
         {
             button2.SetActive(false);
         }
 
-        if (encounterButton3Text != null)
+        if (encounterButton3Text != null || encounterButton3Text != "")
         {
             button3TextSlot.GetComponent<Text>().text = encounterButton3Text;
         }
@@ -77,7 +105,7 @@ public class Encounter : MonoBehaviour
             button3.SetActive(false);
         }
 
-        if (encounterButton4Text != null)
+        if (encounterButton4Text != null || encounterButton4Text != "")
         {
             button4TextSlot.GetComponent<Text>().text = encounterButton4Text;
         }
