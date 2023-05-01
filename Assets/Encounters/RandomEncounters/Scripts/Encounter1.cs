15beedcf713edf6e6bfef4403409f1528c694279
Mr. Beast <lavafrita@gmail.com>
2023-05-01 15:05:25 +0200

    Ya no es un arreglo rapido
    He reordenado como funcionan los eventos, ahora es mucho mas limpio

---
+++ Assets/Encounters/RandomEncounters/Scripts/Encounter1.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Encounter1 : MonoBehaviour
+{
+    public void functionButton1()
+    {
+        Debug.Log("Funciona functionButton1!!!");
+    }
+    public void functionButton3()
+    {
+        Debug.Log("Bals");
+    }
+}
