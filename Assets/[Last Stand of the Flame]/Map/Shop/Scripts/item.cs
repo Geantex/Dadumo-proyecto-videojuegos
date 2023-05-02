d00d66e78478a1eea430deffacdeb7f5d69363ff
Mario Climent <mclijim@epsg.upv.es>
2023-05-02 07:31:19 +0200

    Tienda Finalizada

--- Assets/[Last Stand of the Flame]/Map/Shop/Scripts/item.cs
+++ Assets/[Last Stand of the Flame]/Map/Shop/Scripts/item.cs
@@ -10,13 +10,13 @@ public class item : ScriptableObject
     public string itemName;
     public int itemPrice; // los objetos de la tienda reduciran el dinero del jugador (para comprarlo)
     // los objetos de cofres de tesoros
-    public Texture2D itemImage;
+    public Sprite itemImage;
     public int itemDamageModifier; // esto modificara el daño del personaje
     public int itemHealthModifier; // esto modificara la salud del personaje
     public int itemTier; // esto lo usaremos para saber cómo de fuerte es un objeto, y para que no salgan de menor calidad (1)*
     public string characterTag; // Esto lo usaremos para que solo salgan objetos de personajes que hayan en el juego
     public string itemSlot; // esto solo puede ser o "Weapon" o "Armor"
-    public Texture2D characterImage; // podemos poner una imagen en el canvas que señale quien puede equiparse el objeto
+    public Sprite characterImage; // podemos poner una imagen en el canvas que señale quien puede equiparse el objeto
 
 
     // (1)* Si un objeto está equipado, no debería volver a salir en esta partida. Tampoco deberían de salir objetos de menor tier
