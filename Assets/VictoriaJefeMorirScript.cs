51bc859d61b5f271635a65e715c9df892dc96e00
Rekken4848 <hugomarescrihuela@gmail.com>
2023-06-19 14:55:05 +0200

    FX atras

--- Assets/VictoriaJefeMorirScript.cs
+++ Assets/VictoriaJefeMorirScript.cs
@@ -49,6 +49,5 @@ public class VictoriaJefeMorirScript : MonoBehaviour
         FadeToBlack.QuickFade();
         yield return new WaitForSeconds(0.26f);
         SceneManager.LoadScene("MainMenu");
-        GameObject.Find("StartButton").GetComponent<Button>().interactable = false;
     }
 }
